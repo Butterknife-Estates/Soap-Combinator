f433a80a4e67c6fe486d8f8427f595b67a75a62b
Bret Spencer <stickman.inthe.gallows@gmail.com>
2012-01-13 20:02:11 -0700

    added stuff

--- soap.py
+++ soap.py
@@ -1,13 +1,17 @@
 #Soap Combinator program
 #Butterknife Estates
 from Tkinter import *
+
 master = Tk()
 
-w = Canvas(master, width=500, height=800)
-oilnum = Spinbox(master, from_=0, to=10)
-oilnum.set(4)
+root = Canvas(master, width=50, height=100)
+master.title("Oil Calculator")
+oilnum = Spinbox(master, from_=5, to=10, width=10)
+#oilnum.set(4)
+oilsave=oilnum.get()
 oiltarget=160
-targ = Entry(master, width=100, textvariable=oiltarget)
+targ = Entry(master, width=10, textvariable=oiltarget)
+targ.insert(0,160)
 oilnum.grid(row=0, column=0)
 targ.grid(row=0, column=1)
 
@@ -16,25 +20,44 @@ locked=[]
 value=[]
 valueent=[]
 lockb=[]
-for i in range(oilnum.get()):
+for i in range(int(oilnum.get())):
     scales.append(Scale(master, from_=0, to=100, orient=HORIZONTAL, resolution=0.5))
-    scales.set(100.0/oilnum.get())
+    scales[i].set(100.0/int(oilnum.get()))
     locked.append(False)
-    value.append(100.0/oilnum.get())
     lockb.append(Checkbutton(master, text="Lock", variable=locked))
-    valueent.append(Entry(master, width=60, textvariable=value))
-    
-    scales.grid(row=i+1, columnspan=3)
-    lockb.grid(row=i+1, column=3)
-    valueent.grib(row=i+1, column=4)
+    valueent.append(Entry(master, width=10))
+    valueent[i].insert(0,100/int(oilnum.get()))
+    scales[i].grid(row=i+1, column=0)
+    lockb[i].grid(row=i+1, column=1)
+    valueent[i].grid(row=i+1, column=2)
 
-while 1:
-    for i in range(oilnum.get()):
-        
+#while 1:
+ #   for i in range(oilnum.get()):
 
+	
 
-
-    
-w.update()    
-
-mainloop()
+def update():
+	if oilsave<oilnum.get():
+	    i=oilsave+1
+	    scales.append(Scale(master, from_=0, to=100, orient=HORIZONTAL, resolution=0.5))
+	    scales[i].set(100.0/int(oilnum.get()))
+	    locked.append(False)
+	    lockb.append(Checkbutton(master, text="Lock", variable=locked))
+	    valueent.append(Entry(master, width=10))
+	    valueent[i].insert(0,100/int(oilnum.get()))
+	    scales[i].grid(row=i+1, column=0)
+	    lockb[i].grid(row=i+1, column=1)
+	    valueent[i].grid(row=i+1, column=2)
+            oilsave+=1
+	if oilsave>oilnum.get():
+	    scales.pop()
+	    lockb.pop()
+	    valueent.pop()
+	    scales.pop()
+	    locked.pop()
+	    oilsave-=1
+	for i in range(oilsave);
+	    if ~locked[i]:
+		
+def updateVol():
+root.mainloop()
